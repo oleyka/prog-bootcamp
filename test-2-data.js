02813f0fdf38dfe781d33dec12731c0161274a14
Olga Bobrova <oleyka@gmail.com>
2013-02-12 06:41:21 -0800

    002: solution 1

---
+++ test-2-data.js
@@ -0,0 +1,22 @@
+var KennelData = [ {
+	breed: 'GSD',
+	dogs: [	{ name: 'Aston',
+			  id: 123,
+			  features: { age: 16, sex: 'M', honors: [ { name: "President's Award", years: [2008, 2012, 2014] } ] }
+			},
+			{ name: 'Martin',
+			  id: 135,
+			  features: { age: 8, sex: 'M', honors: [ { name: "President's Award", years: [2011, 2013] } ] }
+			}],
+	}, {
+	breed: 'York',
+	dogs: [	{ name: 'Procter',
+			  id: 234,
+			  features: { age: 4, sex: 'M', honors: [ { name: "Best beggar", years: [2010] } ] }
+			},
+			{ name: 'Gamble',
+			  id: 246,
+			  features: { age: 2, sex: 'M', honors: [] }
+			}],
+	}	
+];
