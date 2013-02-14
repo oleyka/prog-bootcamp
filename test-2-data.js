e4a385d9faa4bb4887b67f6e29514bd0ab25d431
Olga Bobrova <oleyka@gmail.com>
2013-02-13 23:42:24 -0800

    003: task complete

--- test-2-data.js
+++ test-2-data.js
@@ -20,3 +20,26 @@ var kennel_data = [ {
 			}],
 	}	
 ];
+
+var kennel_data2 = [ {
+	breed: 'GSD',
+	dogs: [	{ name: 'Aston',
+			  id: 123,
+			  features: { age: 16, sex: 'M', honors: [ { name: "President's Award", years: [2008, 2012, 2014] } ] }
+			},
+			{ name: 'Martin',
+			  id: 135,
+			  features: { age: 8, sex: 'M', honors: [ { name: "President's Award", years: [2011, 2013] } ] }
+			}],
+	}, {
+	breed: 'York',
+	dogs: [	{ name: 'Procter',
+			  id: 234,
+			  features: { age: 4, sex: 'M', honors: [ { name: "Best beggar", years: [2010, 2012] } ] }
+			},
+			{ name: 'Gamble',
+			  id: 246,
+			  features: { age: 2, sex: 'M', honors: [] }
+			}],
+	}	
+];
