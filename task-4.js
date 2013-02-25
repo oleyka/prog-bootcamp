62ccf5817c5de393fbf9c9bbfe56f2404fee8c58
Olga Bobrova <oleyka@gmail.com>
2013-02-24 22:16:11 -0800

    004: functions cleanup

--- task-4.js
+++ task-4.js
@@ -18,37 +18,26 @@ function dropElementChildrenIds(element) {
 	return element;
 }
 
-function cloneVariantA(domElement) { // using cloneNode()
-	if(domElement) {
-		var clonedElement = domElement.cloneNode(true);
+function clone(domElement) {
+	if(domElement.tagName) {
+		var clonedElement = document.createElement(domElement.tagName);
+		
+		for(var i = 0; i < domElement.attributes.length; i++) {
+			clonedElement.setAttribute(domElement.attributes[i].name, domElement.attributes[i].value);
+		}
+		
+		var childCount = domElement.childNodes ? domElement.childNodes.length : 0;
+		for(var i = 0; i < childCount; i++) {
+			clonedElement.appendChild(clone(domElement.childNodes[i]));
+		}
 		return clonedElement;
 	} else {
-		throw("No element to clone");
+		return document.createTextNode(domElement.data);
 	}
 }
 
-function cloneVariantB(domElement) {
-	if(!domElement || !domElement.tagName) return NULL;
-	var clonedElement = document.createElement(domElement.tagName);
-	
-	for(var i = 0; i < domElement.attributes.length; i++) {
-		clonedElement.setAttribute(domElement.attributes[i].name, domElement.attributes[i].value);
-	}
-	
-	var childCount = domElement.childNodes? domElement.childNodes.length : 0;
-	for(var i = 0; i < childCount; i++) {
-		if(domElement.childNodes[i].tagName) {
-			var cloneChild = cloneVariantB(domElement.childNodes[i]);
-		} else {
-			var cloneChild = document.createTextNode(domElement.childNodes[i].data);
-		}
-		clonedElement.appendChild(cloneChild);
-	}
-	return clonedElement;
-}
-
-function clearClone(domElement, cloneFunction) {
-	var clonedElement = cloneFunction(domElement)
+function cloneAndRename(domElement, cloneFunction) {
+	var clonedElement = cloneFunction(domElement);
 	
 	if(clonedElement.hasAttribute('id')) {
 		clonedElement.setAttribute('id', genUniqueId(clonedElement.attributes['id'].value));
