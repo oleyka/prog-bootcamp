241e5f1f507dcd227e0566cece434399fd4c3b3c
Olga Bobrova <oleyka@gmail.com>
2013-03-05 12:43:52 -0800

    task 6 done

---
+++ task-6.js
@@ -0,0 +1,29 @@
+function rotateAngle(element, angle) {
+    var rotateCSSProperty = "rotate(" + angle + "deg)";
+    setBrowserIndependentProperty(element, "transform", rotateCSSProperty);
+
+    return true;
+}
+
+function balanceElement(element) {
+
+	var maxAngle = 12;
+
+	if (!element.myCount) { 
+		element.myAngle = 0; 
+		element.myCount = 0;
+	} 
+	
+	var newAngle = Math.sin(element.myCount/25) * maxAngle;
+	
+	element.myAngle = newAngle;
+	element.myCount++;
+	
+	console.log(newAngle);
+	
+	rotateAngle(element, newAngle);
+}
+
+function Wobbler(element) {
+	element.onload = setInterval(function() { balanceElement(element) }, 16);
+}
