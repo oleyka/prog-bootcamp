50039d495c440a0e67f44bafc367bb8c4a0a3969
Olga Bobrova <oleyka@gmail.com>
2013-03-23 23:59:46 -0700

    008: 1st version completed

--- task-8/my/my-restaurant-model.js
+++ task-8/my/my-restaurant-model.js
@@ -1,22 +1,40 @@
-// To-Do - открывание заново неоткрытого ресторана?
-
 var Visitor = Backbone.Model.extend({
-    defaults: { lastFoursquareCheckin: "", cid: 0 },
+    defaults: { lastFoursquareCheckin: "" },
+
+    initialize: function() {
+        this.set('cid', this.cid);
+    },
 
     checkin: function(venue) {
         this.set('lastFoursquareCheckin', venue);
     }
 });
 
-// Создаём простую коллекцию посетителей.
 var VisitorsList = Backbone.Collection.extend({ model: Visitor });
 
+var VisitorView = Backbone.View.extend({
+    className: "rVisitor",
+
+    tmpl: _.template($("#visitor-message").html()),
+
+  initialize: function() {
+    this.model.view = this;
+  },
+
+  render: function() {
+    this.$el.html(this.tmpl(this.model.attributes));
+    $(".rClass").append(this.$el); // how about 2 restaurants on the same screen
+    return this;
+  }
+});
+
+/////////////////////////////////////////////////////////
 var RestaurantModel = Backbone.Model.extend({
     defaults: {
         currentOccupancy: 0,
         name: "",
         state: "closed",
-        todayCount: 0,
+        maxOccupancy: 0
     },
     
     initialize: function() {
@@ -45,19 +63,11 @@ var RestaurantModel = Backbone.Model.extend({
     },
 
     visitorCame: function(visitor) {
-        var newVisitor = visitor;
-        if (!visitor) { newVisitor = new Visitor; }
-        if(this.get('state') == "open" && !this.get('visitors').get(newVisitor)) {
-
+        if(this.get('state') == "open" && !this.get('visitors').get(visitor)) {
+            visitor.checkin(this.get('restaurantName'));
+            this.get('visitors').add(visitor);
             this.set({currentOccupancy: this.get('currentOccupancy') + 1 });
-            this.set({todayCount: this.get('todayCount') + 1 });
-
-            newVisitor.set('cid', this.get('todayCount'));
-            newVisitor.checkin(this.get('restaurantName'));
-
-            this.get('visitors').add(newVisitor);
-
-            return this;
+            return visitor;
         }
         return false;
     },
@@ -65,8 +75,8 @@ var RestaurantModel = Backbone.Model.extend({
     visitorLeft: function(visitor) {
         var visitors = this.get('visitors');
         if(visitors.get(visitor)) {
-            this.set({currentOccupancy: this.get('currentOccupancy') - 1 });
             this.get('visitors').remove(visitor);
+            this.set({currentOccupancy: this.get('currentOccupancy') - 1 });
 
             if(this.get('currentOccupancy') == 0 && this.get('state') == "closing") {
                 this.closeRestaurant();
@@ -79,7 +89,6 @@ var RestaurantModel = Backbone.Model.extend({
     openRestaurant: function() {
         var visitors = this.get('visitors');
         if(this.get('state') == "closed") {
-            this.set('todayCount', 0);
             return this.set({'state': "open", 'currentOccupancy': 0}, {validate: true});
         } else {
             console.log("Cannot open unclosed restaurant");
@@ -98,7 +107,6 @@ var RestaurantModel = Backbone.Model.extend({
         }
         return false;
     }
-
 });
 
 var RestaurantView = Backbone.View.extend({
@@ -108,7 +116,10 @@ var RestaurantView = Backbone.View.extend({
   tmpl: _.template($("#restaurant-message").html()),
 
   events: {
-    'click': "do_close"
+    'click .rOpen': 'do_open',
+    'click .rClose': 'do_close',
+    'click .rNewVisitor': 'add_visitor',
+    'click .rVisitor input': 'remove_visitor'
   },
     
   initialize: function() {
@@ -116,15 +127,33 @@ var RestaurantView = Backbone.View.extend({
     this.render();
   },
 
-  // обновить this.el новым содержимым.
   render: function() {
-//    console.log(this.$el);
     this.$el.html(this.tmpl(this.model.attributes));
-    return this;    // Рекомендуется.
+    var visitors = this.model.get('visitors');
+    visitors.each(function(m) { m.view.render(); });
+    return this;
+  },
+
+  do_open: function() {
+    this.model.openRestaurant();
   },
 
   do_close: function() {
     this.model.closeRestaurant();
+  },
+
+  add_visitor: function() {
+    var visitor = new Visitor;
+    var visView = new VisitorView({model: visitor});
+    this.model.visitorCame(visitor);
+  },
+
+  remove_visitor: function(el) {
+    var visitor = this.model.get('visitors').get(el.currentTarget.id);
+    if(visitor) {
+        this.model.visitorLeft(visitor);
+        return true;
+    }
+    return false;
   }
 });
-
