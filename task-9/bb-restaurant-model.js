eac5dcc08e2135c32ae8653aebe25d34c5f34fdf
Olga Bobrova <oleyka@gmail.com>
2013-04-04 11:02:09 -0700

    009: getting there

--- task-9/bb-restaurant-model.js
+++ task-9/bb-restaurant-model.js
@@ -1,16 +1,116 @@
 var Visitor = Backbone.Model.extend({
-    defaults: { lastFoursquareCheckin: "", lastName: "" },
+    defaults: { className: "Visitor", lastName: "", objectId: "" },
 
     initialize: function() {
         this.set('cid', this.cid);
     },
 
-    checkin: function(venue) {
-        this.set('lastFoursquareCheckin', venue);
-    }
+    sync: function(method) { 
+        var self = this;
+        var selectAttr = [ 'lastName', 'cid' ];
+        var baseUrl = "https://api.parse.com/1/classes/";
+
+        var ajaxParams = {
+            'url': baseUrl + this.get('className'),
+            'contentType': 'application/json',
+            'dataType': 'json',
+            'headers': { 'X-Parse-Application-Id': "sUsP9yfDdLAMDqAsFurw6YrVvnhZ3OsbMbePLcQW", 
+                         'X-Parse-REST-API-Key': "IXhN9vJahmqqQoqRxmm9aNIVHNiJSU90ypSlko9G" },
+
+            success: function(data, status, jqXHR) { self.completeSync(data, method); },
+            error: function(jqXHR, errStr, errThrown) { self.failSync(errStr + ": " + errThrown); },
+            complete: function(jqXHR, status) {}
+        };
+
+        switch (method) {
+        case 'create':
+            ajaxParams.type = 'POST';
+            ajaxParams.data = JSON.stringify(this.attributes, selectAttr);
+            break;
+
+        case 'update':
+            ajaxParams.type = 'PUT';
+            ajaxParams.data = JSON.stringify(this.attributes, selectAttr);
+            ajaxParams.url += '/' + this.get('objectId');
+            break;
+
+        case 'delete':
+            ajaxParams.type = 'DELETE';
+            ajaxParams.url += '/' + this.get('objectId');
+            break;
+
+        case 'read':
+            ajaxParams.type = 'GET';
+            ajaxParams.url += '/' + this.get('objectId');
+            break;
+        }
+        console.log('Running ' + ajaxParams.type + ": " + ajaxParams.data);
+        $.ajax(ajaxParams);
+    },
+
+    completeSync: function(data, method) { 
+        console.log("Completed " + method);
+
+        var baseUrl = "https://api.parse.com/1/classes/";
+        var self = this;
+
+        switch (method) {
+        case 'create':
+            this.set('objectId', data.objectId); 
+
+            var rest = this.get('restaurant');
+            var ajaxParams = {
+                'url': baseUrl + rest.get('className') + '/' + rest.get('objectId'),
+                'contentType': 'application/json',
+                'dataType': 'json',
+                'headers': { 'X-Parse-Application-Id': "sUsP9yfDdLAMDqAsFurw6YrVvnhZ3OsbMbePLcQW", 
+                             'X-Parse-REST-API-Key': "IXhN9vJahmqqQoqRxmm9aNIVHNiJSU90ypSlko9G" },
+                'type': "PUT", 
+                data: JSON.stringify({'__op': "AddUnique", 'visitors': [ rest.get('objectId') ] }),
+                success: function(data, status, jqXHR) { },
+                error: function(jqXHR, errStr, errThrown) { self.failSync(errStr + ": " + errThrown); },
+                complete: function(jqXHR, status) {}
+            };
+            console.log('Running ' + ajaxParams.type + ": " + ajaxParams.data);
+            $.ajax(ajaxParams);
+            break; 
+        case 'read':
+            this.set('cid', data.cid);
+            this.set('lastName', data.lastName);
+            break;
+        case 'update': break;
+        case 'delete': 
+            var rest = this.get('restaurant');
+            var ajaxParams = {
+                'url': baseUrl + rest.get('className') + '/' + rest.get('objectId'),
+                'contentType': 'application/json',
+                'dataType': 'json',
+                'headers': { 'X-Parse-Application-Id': "sUsP9yfDdLAMDqAsFurw6YrVvnhZ3OsbMbePLcQW", 
+                             'X-Parse-REST-API-Key': "IXhN9vJahmqqQoqRxmm9aNIVHNiJSU90ypSlko9G" },
+                'type': "PUT", 
+                data: JSON.stringify({'__op': "Remove", 'visitors': [ rest.get('objectId') ] }),
+                success: function(data, status, jqXHR) { },
+                error: function(jqXHR, errStr, errThrown) { self.failSync(errStr + ": " + errThrown); },
+                complete: function(jqXHR, status) {}
+            };
+            console.log('Running ' + ajaxParams.type + ": " + ajaxParams.data);
+            $.ajax(ajaxParams);
+            break;
+        default: break;
+        }
+    },
+    failSync: function(str) { console.log(str); }
 });
 
-var VisitorsList = Backbone.Collection.extend({ model: Visitor });
+var VisitorsList = Backbone.Collection.extend({ 
+    model: Visitor,
+    getByObjectId: function(objectId) {
+        this.each(function(v){ 
+            if (v.get('objectId') === objectId) { return v; }
+        });
+        return false;
+    }
+});
 
 var VisitorView = Backbone.View.extend({
     className: "rVisitor",
@@ -30,10 +130,13 @@ var VisitorView = Backbone.View.extend({
 /////////////////////////////////////////////////////////
 var RestaurantModel = Backbone.Model.extend({
     defaults: {
+        className: 'Restaurant',
         currentOccupancy: 0,
         name: "",
         state: "closed",
-        maxOccupancy: 0
+        maxOccupancy: 0,
+        objectId: "",
+        syncInProgress: 0
     },
     
     initialize: function() {
@@ -61,11 +164,93 @@ var RestaurantModel = Backbone.Model.extend({
         }
     },
 
+    sync: function(method, visitor) { 
+        var self = this;
+        var selectAttr = [ 'currentOccupancy', 'name', 'state', 'maxOccupancy' ];
+        var baseUrl = "https://api.parse.com/1/classes/";
+        var ajaxParams = {
+            'url': baseUrl + this.get('className'),
+            'contentType': 'application/json',
+            'dataType': 'json',
+            'headers': { 'X-Parse-Application-Id': "sUsP9yfDdLAMDqAsFurw6YrVvnhZ3OsbMbePLcQW", 
+                         'X-Parse-REST-API-Key': "IXhN9vJahmqqQoqRxmm9aNIVHNiJSU90ypSlko9G" },
+            success: function(data, status, jqXHR) { self.completeSync(data, method, visitor); },
+            error: function(jqXHR, errStr, errThrown) { self.failSync(errStr + ": " + errThrown); },
+            complete: function(jqXHR, status) {}
+        };
+
+        switch (method) {
+        case 'create':
+            ajaxParams.type = 'POST';
+            ajaxParams.data = JSON.stringify(this.attributes, selectAttr);
+            break;
+
+        case 'update':
+        case 'addVisitor':
+        case 'removeVisitor':
+            ajaxParams.type = 'PUT';
+            ajaxParams.data = JSON.stringify(this.attributes, selectAttr);
+            ajaxParams.url += '/' + this.get('objectId');
+            break;
+
+        case 'delete':
+            ajaxParams.type = 'DELETE';
+            ajaxParams.url += '/' + this.get('objectId');
+            break;
+
+        case 'read':
+            ajaxParams.type = 'GET';
+            ajaxParams.url += '/' + this.get('objectId');
+            break;
+        }
+        console.log('Running ' + ajaxParams.type + ": " + ajaxParams.data);
+        $.ajax(ajaxParams);
+    },
+
+    completeSync: function(data, method, visitor) { 
+        console.log("Completed " + method);
+
+        switch (method) {
+        case 'create': 
+            this.set('objectId', data.objectId); 
+            var visitors = this.get('visitors');
+            visitors.each(function(v) {
+                if (v.get('objectId') === "") { 
+                    v.sync('create'); 
+                // on completion of sync, visitor will add itself to visitors[] on Parse
+                } else { v.sync('update'); }
+            });
+            break;
+        case 'update': break;
+        case 'addVisitor': 
+            visitor.sync('create'); 
+            break;
+        case 'removeVisitor': 
+            if (visitor.get('objectId') !== "") { visitor.sync('delete'); }
+            break;
+        case 'delete': 
+        // this doesn't delete existing visitors from Parse!
+            break;
+        case 'read': 
+            this.set('currentOccupancy', data.currentOccupancy);
+            this.set('maxOccupancy', data.maxOccupancy);
+            this.set('name', data.name);
+            this.set('state', data.state);
+            for (var j = 0; j < this.get('currentOccupancy'); j++) {
+                var visitor = new Visitor({ 'objectId': data.visitors[j] });
+                visitor.sync('read');
+            }
+        default: break;
+        }        
+    },
+    failSync: function(str) { console.log(str); },
+
     visitorCame: function(visitor) {
         if(this.get('state') == "open" && !this.get('visitors').get(visitor)) {
-            visitor.checkin(this.get('restaurantName'));
             this.get('visitors').add(visitor);
+            visitor.set('restaurant', this);
             this.set({currentOccupancy: this.get('currentOccupancy') + 1 });
+            this.sync('addVisitor', visitor);
             return visitor;
         }
         return false;
@@ -80,6 +265,7 @@ var RestaurantModel = Backbone.Model.extend({
             if(this.get('currentOccupancy') == 0 && this.get('state') == "closing") {
                 this.closeRestaurant();
             }
+            this.sync('removeVisitor', visitor);
             return this;
         }
         return false;
@@ -88,11 +274,12 @@ var RestaurantModel = Backbone.Model.extend({
     openRestaurant: function() {
         var visitors = this.get('visitors');
         if(this.get('state') == "closed") {
-            return this.set({'state': "open", 'currentOccupancy': 0}, {validate: true});
-        } else {
-            console.log("Cannot open unclosed restaurant");
-            return false;
+            this.set({'state': "open", 'currentOccupancy': 0}, {validate: true});
+            this.sync('update');
+            return this;
         }
+        console.log("Cannot open unclosed restaurant");
+        return false;
     },
 
     closeRestaurant: function() {
@@ -102,6 +289,7 @@ var RestaurantModel = Backbone.Model.extend({
             } else {
                 this.set({'state': "closing"}, {validate: true});
             }
+            this.sync('update');
             return this;
         }
         return false;
