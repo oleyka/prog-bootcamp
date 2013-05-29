40cefbcbbd5a2ad37a8573fa921d7d2f89af83e7
Olga Bobrova <oleyka@gmail.com>
2013-05-28 17:18:26 -0700

    10: initial task, with sub-functions

--- task-10/my/myButton.js
+++ task-10/my/myButton.js
@@ -11,17 +11,15 @@ define(
       this.defaultAttrs({ value: 'Click me for time' });
 
       this.updateText = function() { 
-          console.log('x');
           this.trigger('#myText', 'update');
       }
 
       this.after('initialize', function() {
-        console.log('y');
         this.updateText();
+
         this.node.value = this.attr.value;
         this.on('click', this.updateText);      
       });
-      console.log('0');
     }
   }
 );
--- task-10/my/myText.js
+++ task-10/my/myText.js
@@ -11,23 +11,19 @@ define(
       this.defaultAttrs({ text: "Click that button for time ->" });
 
       this.showText = function() {
-        console.log("c");
         this.$node.text(this.attr.text);
       }
 
       this.updateTime = function() { 
-        console.log("b");
         var now = new Date();
         this.attr.text = now.getHours() + ":" + now.getMinutes() + ':' + now.getSeconds();
         this.showText();
       }
 
       this.after('initialize', function() {
-        console.log("a");
         this.showText();
         this.on('update', this.updateTime);
       });
-      console.log('000');
     }
   }
 );
